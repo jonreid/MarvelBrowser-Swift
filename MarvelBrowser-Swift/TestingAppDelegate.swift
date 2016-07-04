d84cd35748f348161be3d54029f56491c848b7e8
Jon Reid <jon@qualitycoding.org>
2016-07-04 16:19:57 -0700

    TestingAppDelegate does need window property to quiet logged warning

--- MarvelBrowser-Swift/TestingAppDelegate.swift
+++ MarvelBrowser-Swift/TestingAppDelegate.swift
@@ -1,7 +1,8 @@
 //  TDD sample app MarvelBrowser-Swift by Jon Reid, http://qualitycoding.org/about/
 //  Copyright 2016 Jonathan M. Reid. See LICENSE.txt
 
-import Foundation
+import UIKit
 
 class TestingAppDelegate: NSObject {
+    var window: UIWindow?
 }
