ada7b0a14237b6c767b16ef8c023eb3ed03c9949
Jon Reid <jon@qualitycoding.org>
2016-08-06 14:59:15 -0700

    Simplest timeinterval code to pass

--- MarvelBrowser-Swift/MarvelAuthentication.swift
+++ MarvelBrowser-Swift/MarvelAuthentication.swift
@@ -1,10 +1,12 @@
 //  TDD sample app MarvelBrowser-Swift by Jon Reid, http://qualitycoding.org/about/
 //  Copyright 2016 Jonathan M. Reid. See LICENSE.txt
 
+import Foundation
+
 struct MarvelAuthentication {
     
     func timestamp() -> String {
-        return ""
+        return String(Date.init().timeIntervalSinceReferenceDate)
     }
     
 }
