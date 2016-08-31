ef91b6758cccfaff835a3d5589ffd334dd94d714
Jon Reid <jon@qualitycoding.org>
2016-08-30 18:03:24 -0700

    Revert "Change md5 from closure property to standalone function, injected as default parameter"
    This reverts commit 4e8291df05bbdda61c255a056090bce12f9a6bdf.

--- MarvelBrowser-Swift/MarvelAuthentication.swift
+++ MarvelBrowser-Swift/MarvelAuthentication.swift
@@ -3,29 +3,26 @@
 
 import Foundation
 
-func md5(_ str: String) -> String {
-    var digest = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-    CC_MD5(str, CC_LONG(str.utf8.count), &digest)
-    return digest.reduce("") {
-        $0 + String(format: "%02x", $1)
-    }
-}
-
 struct MarvelAuthentication {
-
+    
     var publicKey = MarvelKey.`public`
     var privateKey = MarvelKey.`private`
+    
+    var md5: (String) -> String = { str in
+        var digest = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+        CC_MD5(str, CC_LONG(str.utf8.count), &digest)
+        return digest.reduce("") {
+            $0 + String(format: "%02x", $1)
+        }
+    }
 
-    func urlParameters(
-            timestamp: String = MarvelAuthentication.timestamp(),
-            md5: (String) -> String = md5)
-                    -> String {
+    func urlParameters(timestamp: String = MarvelAuthentication.timestamp()) -> String {
         let hash = md5(timestamp + privateKey + publicKey)
         return "&ts=\(timestamp)&apikey=\(publicKey)&hash=\(hash)"
     }
-
+    
     private static func timestamp() -> String {
         return String(Date.init().timeIntervalSinceReferenceDate)
     }
-
+    
 }
