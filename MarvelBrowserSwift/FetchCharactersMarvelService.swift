8fe11ecfe04f1a28f34ccab0d925cfcd52c75009
Jon Reid <jon@qualitycoding.org>
2016-11-06 15:13:01 -0800

    Beginning of FetchCharactersMarvelService init, specifying URLSession which isn't quite what we want

---
+++ MarvelBrowserSwift/FetchCharactersMarvelService.swift
@@ -0,0 +1,10 @@
+//  TDD sample app MarvelBrowser-Swift by Jon Reid, http://qualitycoding.org/about/
+//  Copyright 2016 Jonathan M. Reid. See LICENSE.txt
+
+import Foundation
+
+class FetchCharactersMarvelService {
+    init(session: URLSession) {
+
+    }
+}
--- MarvelBrowserSwift/FetchCharactersMarvelServiceTests.swift
+++ MarvelBrowserSwift/FetchCharactersMarvelServiceTests.swift
@@ -1,10 +1,13 @@
+//  TDD sample app MarvelBrowser-Swift by Jon Reid, http://qualitycoding.org/about/
+//  Copyright 2016 Jonathan M. Reid. See LICENSE.txt
+
 import XCTest
 @testable import MarvelBrowserSwift
 
 class FetchCharactersMarvelServiceTests : XCTestCase {
 
-    func testExample() {
-        XCTFail("Tests not yet implemented in FetchCharactersMarvelServiceTests")
+    func testFetchCharacters_ShouldMakeDataTaskForMarvelEndpoint() {
+        let sut = FetchCharactersMarvelService(session: URLSession.shared)
     }
-    
+
 }
