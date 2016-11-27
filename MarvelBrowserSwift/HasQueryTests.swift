b4132eb5a6bdb784c7eac8aa2b229422baa32c6b
Jon Reid <jon@qualitycoding.org>
2016-11-26 17:33:59 -0800

    Add missing copyright file headers

--- MarvelBrowserSwift/HasQueryTests.swift
+++ MarvelBrowserSwift/HasQueryTests.swift
@@ -1,3 +1,6 @@
+//  TDD sample app MarvelBrowser-Swift by Jon Reid, http://qualitycoding.org/about/
+//  Copyright 2016 Jonathan M. Reid. See LICENSE.txt
+
 import XCTest
 
 class HasQueryTests : XCTestCase {
