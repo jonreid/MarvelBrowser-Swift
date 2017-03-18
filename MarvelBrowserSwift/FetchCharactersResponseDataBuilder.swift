d8307488e93b1ba174ffc99f49295cf352a2d570
Jon Reid <jon@qualitycoding.org>
2017-03-18 16:47:20 -0700

    Generate empty class to satisfy test

---
+++ MarvelBrowserSwift/FetchCharactersResponseDataBuilder.swift
@@ -0,0 +1,5 @@
+//  TDD sample app MarvelBrowser-Swift by Jon Reid, http://qualitycoding.org/about/
+//  Copyright 2016 Jonathan M. Reid. See LICENSE.txt
+
+class FetchCharactersResponseDataBuilder {
+}
