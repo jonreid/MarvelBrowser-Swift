bc6a16c6c0bdabfd061d75dfda0596d74eaf5f03
Jon Reid <jon@qualitycoding.org>
2017-03-18 16:50:49 -0700

    Create offset property

--- MarvelBrowserSwift/FetchCharactersResponseDataBuilder.swift
+++ MarvelBrowserSwift/FetchCharactersResponseDataBuilder.swift
@@ -1,7 +1,11 @@
 //  TDD sample app MarvelBrowser-Swift by Jon Reid, http://qualitycoding.org/about/
 //  Copyright 2016 Jonathan M. Reid. See LICENSE.txt
 
+import Foundation
+
 class FetchCharactersResponseDataBuilder {
+    var offset: NSNumber?
+
     func parse(dictionary dict: [String: Any]) {
     }
 }
