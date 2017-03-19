94fdc5f7932b3e3bfc851820037c3bb1efaaf6de
Jon Reid <jon@qualitycoding.org>
2017-03-18 22:29:02 -0700

    Change optionals from var to let

--- MarvelBrowserSwift/FetchCharactersResponseDataBuilder.swift
+++ MarvelBrowserSwift/FetchCharactersResponseDataBuilder.swift
@@ -2,17 +2,17 @@
 //  Copyright 2016 Jonathan M. Reid. See LICENSE.txt
 
 class FetchCharactersResponseDataBuilder {
-    var offset: Int?
-    var total: Int?
-    var results: [CharacterResponseBuilder]?
+    let offset: Int?
+    let total: Int?
+    let results: [CharacterResponseBuilder]?
 
     init(dictionary dict: [String: Any]) {
         offset = dict["offset"] as? Int
         total = dict["total"] as? Int
-        results = parseResults(from: dict["results"] as? Array<[String: Any]>)
+        results = type(of: self).parseResults(from: dict["results"] as? Array<[String: Any]>)
     }
 
-    private func parseResults(from array: Array<[String: Any]>?) -> [CharacterResponseBuilder]? {
+    private static func parseResults(from array: Array<[String: Any]>?) -> [CharacterResponseBuilder]? {
         return array?.map() { return CharacterResponseBuilder(dictionary: $0) }
     }
 
