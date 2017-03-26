ad9a4bf3c8f00b042d85447d238dfaae644ced62
Jon Reid <jon@qualitycoding.org>
2017-03-25 18:12:31 -0700

    Restore production code that passes, but now we have a new test to verify it

--- MarvelBrowserSwift/CharactersSliceResponseBuilder.swift
+++ MarvelBrowserSwift/CharactersSliceResponseBuilder.swift
@@ -25,6 +25,6 @@ struct CharactersSliceResponseBuilder {
     }
 
     private func buildCharacters() -> [CharacterResponseModel] {
-        return results?.flatMap() { $0.build() } ?? [CharacterResponseModel(name: "DUMMY")]
+        return results?.flatMap() { $0.build() } ?? []
     }
 }
