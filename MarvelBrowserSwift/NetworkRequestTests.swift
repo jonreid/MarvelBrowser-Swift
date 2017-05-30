e857af52d5634652df4e0024a5eec15eb50202a4
Jon Reid <jon@qualitycoding.org>
2017-05-29 17:16:52 -0700

    Add "step zero" failing test to prove new test suite is hooked up

---
+++ MarvelBrowserSwift/NetworkRequestTests.swift
@@ -0,0 +1,13 @@
+//  TDD sample app MarvelBrowser-Swift by Jon Reid, http://qualitycoding.org/about/
+//  Copyright 2017 Jonathan M. Reid. See LICENSE.txt
+
+@testable import MarvelBrowserSwift
+import XCTest
+
+class NetworkRequestTests: XCTestCase {
+
+    func testExample() {
+        XCTFail("Tests not yet implemented in NetworkRequestTests")
+    }
+    
+}
